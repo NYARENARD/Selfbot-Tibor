d2645b352b61c2186f6e196165d5426e214db2a0
Vitaliy Kovalev <kovalevvyu@student.bmstu.ru>
2021-05-12 08:22:34 +0300

    Отладка

--- main.py
+++ main.py
@@ -52,8 +52,8 @@ def main(resp):
                 msg_id = m['id']
         self = (m['author']['id'] != id)
 
-        trigger = "TRIGGER"
-        database = "DATABASE"
+        trigger = '"TRIGGER_NAME"'
+        database = '"DATABASE_NAME"'
 
         if self and (channelID == '730552031735054337' or channelID == '801377114905116685'):
             if mentioned:
