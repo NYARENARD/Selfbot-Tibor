454c4c82c75e0910f41f0558a4c8c6346d4b8787
NYARENARD <82282414+NYARENARD@users.noreply.github.com>
2022-01-16 18:08:38 +0300

    backend + cosmetic  fix

--- main.py
+++ main.py
@@ -6,47 +6,54 @@ import os
 from threading import Thread
 
 class Bot:
-    
-    global bot
 
-    token = os.getenv("TOKEN")
-    database = os.getenv("DATABASE_NAME")
-    trigger = os.getenv("TRIGGER_NAME")
-    allowed_channels = os.getenv("ALLOWED_CHANNELS").split()
+    def __init__(self, token, database, trigger, channels):
+        self._token = token
+        self._database = database
+        self._trigger = trigger
+        self._channels = channels
 
-
-    def __init__(self):
-        self.bot = discum.Client(token=self.token, log=False)
+        self.bot = discum.Client(token = self._token, log=False)           #не приват
+        self._thread = Thread(target=self._commands_launch)
+        self._thread.start()
         print("\n>>> Подключение успешно.\n")
-        self.enable_genai()
+        self._enable_genai()
+
+    def __del__(self):
+        self._kill_genai()
+        self.bot.gateway.close()
+        self._thread.join()
+        print("\n>>> Соединение сброшено.\n")
+	
 		
-	#команды включения и отключения бота Genai в определенном канале		
-    def enable_genai(self):
+    def _enable_genai(self):
         self.bot.sendMessage("730552031735054337", "g.interval random")
         time.sleep(1)
         self.bot.sendMessage("730552031735054337", "g.config mention_gen +")
 	
-    def kill_genai(self):
+    def _kill_genai(self):
         self.bot.sendMessage("730552031735054337", "g.interval off")
         time.sleep(1)
         self.bot.sendMessage("730552031735054337", "g.config mention_gen -")
 
 
-    def is_triggered(self, content):
-        with open(self.trigger, 'r', encoding="utf-8") as f:
+    def _is_triggered(self, content):
+        with open(self._trigger, 'r', encoding="utf-8") as f:
             for line in f:
                 if line.strip() in content.lower():
                     return True
-            return False  
+        return False  
 
-    def response(self, channelID):
-        time.sleep(random.randint(7, 15))
+    def _response(self, channelID):
+        time.sleep(random.randint(4, 8))
         self.bot.typingAction(channelID)
-        with open(self.database, 'r', encoding="utf-8") as f:
+        with open(self._database, 'r', encoding="utf-8") as f:
             base_ar = f.read().split('\n')
-            return random.choice(base_ar)
+            to_send = random.choice(base_ar)
+            time.sleep(len(to_send) // 6 + 1)
+            return to_send
 
-    def timestamp_parse(self, raw):
+    def _timestamp_parse(self, raw):
         sec = raw[17:22]
         minu = raw[14:16]
         hr = int(raw[11:13]) + 3
@@ -57,19 +64,24 @@ class Bot:
         timestamp = hr+':'+minu+':'+sec+' '+day+'-'+mon+'-'+year
         return timestamp
 
-    def main_launch(self):
 
-        global bot
+    def _commands_launch(self):
 
         @self.bot.gateway.command
         def reaction_add(resp):
             if resp.event.reaction_added:
                 m = resp.parsed.auto()
                 if m["emoji"]["name"]=='🤙':
-                    time.sleep(random.randint(2, 4))
                     channelID = m["channel_id"]
                     messageID = m["message_id"]
+                    id = self.bot.gateway.session.user["id"]
+                    time.sleep(1)
                     self.bot.addReaction(channelID, messageID, '🤙')
+
+                    himself = (m["member"]["user"]["id"] == id)
+                    if not himself:
+                        print("> {} | {} | 🤙".format(channelID, messageID))
+                    #self.bot.sendMessage("730552031735054337", "> {} | {} | 🤙".format(channelID, messageID))
 
         @self.bot.gateway.command
         def respond(resp):
@@ -80,7 +92,7 @@ class Bot:
                 discriminator = m["author"]["discriminator"]
                 id = self.bot.gateway.session.user["id"]
                 content = m["content"]
-                timestamp = self.timestamp_parse(m["timestamp"])
+                timestamp = self._timestamp_parse(m["timestamp"])
                 
                 mentioned = False
                 for i in m["mentions"]:
@@ -89,41 +101,34 @@ class Bot:
                         msg_id = m["id"]
                 mentioned_towrite = "[MENTIONED] " if mentioned else ''
 
-                triggered = self.is_triggered(content)
+                triggered = self._is_triggered(content)
                 triggered_towrite = "[TRIGGERED] " if triggered else ''
 
                 print("> {}{}{} | {} | {}#{}: {}".format(triggered_towrite, mentioned_towrite, channelID, timestamp, username, discriminator, content))
+                #self.bot.sendMessage("730552031735054337", "> {}{}{} | {} | {}#{}: {}".format(triggered_towrite, mentioned_towrite, channelID, timestamp, username, discriminator, content))
 
                 himself = (m["author"]["id"] == id)
 
-                if not himself and channelID in self.allowed_channels: 
+                if not himself and channelID in self._channels: 
                     if mentioned:
-                        self.bot.reply(channelID, msg_id, self.response(channelID))
+                        self.bot.reply(channelID, msg_id, self._response(channelID))
                     elif triggered:
-                        self.bot.sendMessage(channelID, self.response(channelID))
+                        self.bot.sendMessage(channelID, self._response(channelID))
           
         self.bot.gateway.run(auto_reconnect=True)
 
 
 
 if __name__ == '__main__':
-
-    bot = None
-
+    
     def bot_launch():
-        global bot
-        bot = Bot()
-
-        global thread
-        thread = Thread(target=bot.main_launch)
-        thread.start()
+        global instance
+        instance = Bot(token, database, trigger, channels)
 
     def bot_kill():
-        global bot
-        bot.kill_genai()
-        bot.bot.gateway.close()
-        thread.join()
-        print("\n>>> Соединение сброшено.\n")
+        global instance
+        instance.__del__()
+        instance = None
 
     def MSKtoEU_timezone(time_MSK):
         hour = int(time_MSK[0:2]) - 3
@@ -136,6 +141,10 @@ if __name__ == '__main__':
             hour = str(hour)
         return hour + ':' + minute
 
+    token = os.getenv("TOKEN")
+    database = os.getenv("DATABASE_NAME")
+    trigger = os.getenv("TRIGGER_NAME")
+    channels = os.getenv("ALLOWED_CHANNELS").split()
     
     launch_time = MSKtoEU_timezone(os.getenv("LAUNCH_TIME"))
     kill_time = MSKtoEU_timezone(os.getenv("KILL_TIME"))
