f5820619ddd427ae4ab16131657dd6e0f4da0eca
NYARENARD <82282414+NYARENARD@users.noreply.github.com>
2022-06-11 21:02:59 +0300

    Delete unnecessary features

--- src/config_vars.py
+++ src/config_vars.py
@@ -7,17 +7,5 @@ class_vars = {
     "trigger" : os.getenv("TRIGGER_NAME"),
     "channels" : os.getenv("CHANNELS").split(),
     "prefix" : os.getenv("PREFIX"),
-    "logchannel" : os.getenv("LOGCHANNEL"),
-    "banlist" : os.getenv("BANLIST").split(','),
-    "guildID" : os.getenv("GUILDID"),
-    "channelID_to_fetch" : os.getenv("CHTOFETCH"),
-    "roleID" : os.getenv("ROLEID"),
-    "genai_channel" : os.getenv("GENAI_CHANNEL"),
-    "shame_channel" : os.getenv("SHAME_CHANNEL")
-}
-
-schedule_vars = {
-    "schedule_flag" : os.getenv("SCHEDULE_FLAG"),
-    "launch_time" : os.getenv("LAUNCH_TIME"),
-    "kill_time" : os.getenv("KILL_TIME")
+    "logchannel" : os.getenv("LOGCHANNEL")
 }
