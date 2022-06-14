1d8ee3370d57c7882f215eae2ad6d2c0bc3a80c6
NYARENARD <82282414+NYARENARD@users.noreply.github.com>
2022-06-15 00:30:40 +0300

    Update bot_class.py

--- src/bot_class.py
+++ src/bot_class.py
@@ -28,9 +28,9 @@ class Bot:
         self._thread.start()
         chrome_options = webdriver.ChromeOptions()
         chrome_options.binary_location = cfg["binary_location"]
-        #chrome_options.add_argument("--headless")
-        #chrome_options.add_argument("--disable-dev-shm-usage")
-        #chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--start-maximized")
         self._browser = webdriver.Chrome(executable_path=cfg["executable_location"], chrome_options=chrome_options)
         self._logging(">>> `<@" + self._owner_id + ">`" + " Подключение успешно.", [])
