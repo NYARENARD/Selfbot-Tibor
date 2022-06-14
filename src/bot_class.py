fb16564b82bd593974ebd301e035dd3b14ebc473
NYARENARD <82282414+NYARENARD@users.noreply.github.com>
2022-06-14 15:57:55 +0300

    Update bot_class.py

--- src/bot_class.py
+++ src/bot_class.py
@@ -23,7 +23,12 @@ class Bot:
         self.bot = discum.Client(token = self._token, log=False)
         self._thread = Thread(target=self._commands_launch)
         self._thread.start()
-        self._browser = webdriver.Firefox()
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
+        self._browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
         self._logging("\n>>> Подключение успешно.\n", [])
 
     def __del__(self):
